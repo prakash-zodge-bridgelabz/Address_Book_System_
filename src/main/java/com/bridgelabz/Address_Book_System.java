7a01fda1cf6e33c5a53eca2add80af0c3d57dc3a
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2024-01-08 14:14:47 +0530

    Added: Ensuring there is no duplicate entry of same person in addess book

--- src/main/java/com/bridgelabz/Address_Book_System.java
+++ src/main/java/com/bridgelabz/Address_Book_System.java
@@ -1,4 +1,13 @@
 package com.bridgelabz;
+
+import java.util.ArrayList;
+
+// Use case 7:
+// Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book
+// Duplicate Check is done on Person Name while adding person to Address Book.
+// Use Collection Methods to Search Person by Name for Duplicate Entry Override equals method
+// to check for Duplicate
+// Use Java Streams
 class Contact{
     String first_name,last_name,address,city,state,email,phone_number;
     int zip;
@@ -12,6 +21,26 @@ class Contact{
         this.email = email;
         this.phone_number = phone_number;
         this.zip = zip;
+    }
+    Contact(){
+        //this constructor is created for calling setAllContacts function
+    }
+    ArrayList<Contact> allContacts = new ArrayList<>();
+    void setAllContacts(ArrayList<Contact> allContacts){
+        this.allContacts = allContacts;
+    }
+    String addContact(Contact obj){
+        Contact tempContact = allContacts.stream()
+                .filter(p->p.getFirst_name().equals(obj.first_name))
+                .findFirst()
+                .orElse(null);
+        if(tempContact != null){    //Contact is already present in the allContacts list
+            return (tempContact.first_name+" : Contact is already present in allContacts list");
+        }
+        else{   //Executes when contact not present in the allContacts list
+            allContacts.add(obj);
+            return (obj.first_name+" : Contact is added successfully...");
+        }
     }
     public String getFirst_name() {                 return first_name;              }
     public void setFirst_name(String first_name) {  this.first_name = first_name;   }
--- src/test/java/com/bridgelabz/Test_Address_Book_System.java
+++ src/test/java/com/bridgelabz/Test_Address_Book_System.java
@@ -1,4 +1,48 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+// Use case 7:
+// Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book
+// Duplicate Check is done on Person Name while adding person to Address Book.
+// Use Collection Methods to Search Person by Name for Duplicate Entry Override equals method
+// to check for Duplicate
+// Use Java Streams
 public class Test_Address_Book_System {
+    ArrayList<Contact> allContacts = new ArrayList<>();
+    Contact prakash,nikhil,mrinal,pavan,c;
+    @Before
+    public void init(){
+        prakash = new Contact("Prakash","Zodge","N-5","Aurangabad",
+                "Maharashtra","prakash.zodge.bridgelabz@gmail.com",
+                "9876543210",431003);
+        nikhil = new Contact("Nikhil","Namdev","N-6","Belapur",
+                "Maharashtra","nikhil@gmail.com","9988776655",
+                431004);
+        mrinal = new Contact("Mrinal","Keshav","N-7","Patna",
+                "Bihar","mrinal@gmail.com","9998887776",
+                431005);
+        pavan = new Contact("Pavan","Zore","N-8","Gangapur",
+                "Maharashtra","pavan@@gmail.com","9999888876",
+                431006);
+        allContacts.add(prakash);
+        allContacts.add(nikhil);
+        allContacts.add(mrinal);
+        allContacts.add(pavan);
+        c = new Contact();
+        c.setAllContacts(allContacts);
+    }
+    @Test
+    public void test_ensureNoDuplicate(){
+        Contact himanshu = new Contact("Prakash","Zodge","N-5","Aurangabad",
+                "Maharashtra","prakash.zodge.bridgelabz@gmail.com",
+                "9876543210",431003);
+        String expected = himanshu.first_name+" : Contact is already present in allContacts list";
+        String actual = c.addContact(himanshu);
+        Assert.assertEquals(expected,actual);
+    }
 }
